9a4568bd1c030af36b584005d78b08e5d3e6ff68
Akshat Rawat <akshatrawat84@gmail.com>
2024-04-13 22:18:06 +0530

    Loops.js

---
+++ Week 1.4 Loops , Functions and CallBack Function/Loops.js
@@ -0,0 +1,9 @@
+// loops :-  When we want to write the same thing again and again .
+// Calculate the sum of 0 to 100 .
+
+  let ans = 0 ;
+  for (let i = 1 ; i <= 50 ; i ++){
+    ans = ans  + i ;
+
+  }
+  console.log("Sum is : "  + ans );
