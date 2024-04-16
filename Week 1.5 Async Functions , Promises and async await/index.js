b1598c24d55df495867e74e12dac84359d728acb
Akshat Rawat <akshatrawat84@gmail.com>
2024-04-16 07:32:15 +0530

    update in the code

--- Week 1.5 Async Functions , Promises and async await/index.js
+++ Week 1.5 Async Functions , Promises and async await/index.js
@@ -6,3 +6,7 @@ fs.readFile("a.txt" , "utf-8", function(err,data){
 })
 
 
+console.log("He there ") ; 
+
+// Which one run first ?
+// First Hi there is run then a.txt run . Because of async funcs .
